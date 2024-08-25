64e8f1a56adb43d47fce2582e33f5a45270be646
Mahendra Goud <mahendra.goud@proeffective.in>
2024-08-26 04:40:31 +0530

    some changes

---
+++ src/test/java/com/cgi/jokes/containerTest/MyRepositoryIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.cgi.jokes.containerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJpaTest
+public class MyRepositoryIntegrationTest {
+
+    @Container
+    public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @Autowired
+    private DataSource dataSource;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDB::getUsername);
+        registry.add("spring.datasource.password", postgresDB::getPassword);
+    }
+
+    @Test
+    void testRepository() throws Exception {
+        // Your test logic here, for example:
+        try (var connection = dataSource.getConnection()) {
+            var statement = connection.createStatement();
+            var resultSet = statement.executeQuery("SELECT * from joke");
+            assertThat(resultSet.next()).isTrue();
+        }
+    }
+}
+
